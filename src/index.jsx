330689497a4b63bbe4e2486ab07dcf5f17da091e
vlad4400 <sleepvs@gmail.com>
2020-12-24 16:48:11 +0100

    added 'messages & sender'

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1 className="element">It's works</h1>;
+let messages = ['first message', 'second message'];
 
-ReactDOM.render(
-    element,
-    document.getElementById('root'),
-);
+const MessageComponent = ({ text }) => <div>{ text }</div>;
+
+const MessageField = (props) => {
+    return props.messages.map((message, index) => <MessageComponent key={ index } text={ message }/>);
+};
+
+function saveMessage() {
+    messages.push(document.getElementsByName('message')[0].value);
+    console.log(document.getElementsByName('message')[0].value);
+};
+
+function tick() {
+    const element = (
+        <div> 
+            <MessageField messages={ messages } />
+            <input name="message"></input>
+            <button onClick={ saveMessage }>Send</button> 
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 100);
